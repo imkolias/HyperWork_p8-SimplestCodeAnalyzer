32fefbaa206f019affb959d29c0d28ebbe78f87c
Nik Jakimovec <imkolias@gmail.com>
2023-01-10 11:28:56 +0300

    5/5 major improvements + ast lib use + 3 new errors check + show names of error arg,var,func in log

--- Static Code Analyzer/task/analyzer/code_analyzer.py
+++ Static Code Analyzer/task/analyzer/code_analyzer.py
@@ -1,6 +1,7 @@
 import os
 import re
 import argparse
+import ast
 
 
 errors_dict = {"S001": "Too long",
@@ -9,9 +10,13 @@ errors_dict = {"S001": "Too long",
                "S004": "At least two spaces required before inline comments",
                "S005": "TODO found",
                "S006": "More than two blank lines preceding a code line",
-               "S007": "Too many spaces after 'class'",
-               "S008": "Class name 'user' should use CamelCase",
-               "S009": "Function name 'Print2' should use snake_case"}
+               "S007": "Too many spaces after 'class' or 'func'",
+               "S008": "Class name 'X===X' should use CamelCase",
+               "S009": "Function name 'X===X' should use snake_case",
+               "S010": "Argument name 'X===X' should be written in snake_case",
+               "S011": "Variable 'X===X' should be written in snake_case",
+               "S012": "The default argument value is mutable"}
+
 
 exclude_list = ["__init__.py", "tests.py"]
 
@@ -27,10 +32,10 @@ class BestAnalyzer:
         self.filescount = 0
         self.log_data = {}
 
-    def add_error(self, fname, linenum, code):
+    def add_error(self, fname, linenum, code, wrongplacename=""):
         if (linenum and code) != "":
             if code in errors_dict.keys():
-                self.log_data[self.errornum] = [fname, linenum, code]
+                self.log_data[self.errornum] = [fname, linenum, code, wrongplacename]
         self.errornum += 1
 
     def checklen(self, line, linenum):
@@ -94,44 +99,106 @@ class BestAnalyzer:
         if re_obj:
             self.add_error(self.curr_file, linenum, "S009")
 
+    def ast_funsnake(self, tree):
+        """ S009 : Check if function snake_case """
+        # print(ast.dump(tree))
+        for item in ast.walk(tree):
+            if isinstance(item, ast.FunctionDef) and item.name[0:2] != "__":
+                template = r"_?[a-z]{1,}(_?[a-z]{1,}){0,}"
+                re_obj = re.match(template, item.name)
+                if not re_obj:
+                    self.add_error(self.curr_file, item.lineno, "S009", item.name)
+
+    def ast_argsnakecase(self, tree):
+        """ S010 : Check if func arg is snake_case """
+        for node in ast.walk(tree):
+            if isinstance(node, ast.FunctionDef):
+                for elem in node.args.args:
+                    if not elem.arg.islower():
+                        self.add_error(self.curr_file, elem.lineno, "S010", elem.arg)
+
+
+    def ast_varsnakecase(self, tree):
+        """ S011 : Check if var is snake_case """
+        names_list = []
+        for node in ast.walk(tree):
+            if isinstance(node, ast.Name):
+                if node.id not in names_list:
+                    names_list.append(node.id)
+                    if not node.id.islower():
+                        self.add_error(self.curr_file, node.lineno, "S011", node.id)
+
+    def ast_argmutable(self, tree):
+        """ S012: Check default argument value is mutable """
+        var_list = []
+
+        for node in ast.walk(tree):
+            if isinstance(node, ast.FunctionDef):
+                for elem in node.args.args:
+                    if elem.arg != "self":
+                        var_list.append(elem.arg)
+                for cnt, elem in enumerate(node.args.defaults):
+                    if isinstance(elem, (ast.List, ast.Dict, ast.Set)):
+                        self.add_error(self.curr_file, elem.lineno, "S012", var_list[cnt])
+                var_list.clear()
+
+    def check_code_classic(self, fp):  # check via RegExp
+        linenum = 1
+        for line in fp.readlines():
+            self.checklen(line, linenum)
+            self.check_comment_spaces(line, linenum)
+            self.check_ident_spaces(line, linenum)
+            self.check_semicolor(line, linenum)
+            self.check_todo(line, linenum)
+            self.check_blanklines(line, linenum)
+            self.check_classspaces(line, linenum)
+            self.check_defspaces(line, linenum)
+            self.check_classcamels(line, linenum)
+            linenum += 1
+
+    def check_code_ast(self, tree):  # check via AST lib
+        self.ast_funsnake(tree)
+        self.ast_argsnakecase(tree)
+        self.ast_argmutable(tree)
+        self.ast_varsnakecase(tree)
+
+
     def checkcode(self):
-        temp_list = []
+        temp_flist = []
         if not os.path.exists(self.file_name):
             raise FileNotFoundError
 
         if os.path.isdir(self.file_name):
-            temp_list = [self.file_name+'\\'+fname for fname in os.listdir(self.file_name)
-                         if fname.find(".py") > 1 and fname not in exclude_list]
+            temp_flist = [self.file_name + '\\' + fname for fname in os.listdir(self.file_name)
+                          if fname.find(".py") > 1 and fname not in exclude_list]
 
         elif os.path.isfile(self.file_name):
-            temp_list = [self.file_name]
+            temp_flist = [self.file_name]
 
-        for one_file in temp_list:
+        for one_file in temp_flist:
             if os.path.getsize(one_file) > 0:
                 self.curr_file = one_file.lower()
                 self.filescount += 1
-                linenum = 1
+                # classic RegExp check
                 with open(one_file) as fp:
-                    for line in fp.readlines():
-                        self.checklen(line, linenum)
-                        self.check_comment_spaces(line, linenum)
-                        self.check_ident_spaces(line, linenum)
-                        self.check_semicolor(line, linenum)
-                        self.check_todo(line, linenum)
-                        self.check_blanklines(line, linenum)
-                        self.check_classspaces(line, linenum)
-                        self.check_defspaces(line, linenum)
-                        self.check_classcamels(line, linenum)
-                        self.check_functsnake(line, linenum)
-                        linenum += 1
+                    self.check_code_classic(fp)
+
+                # advanced AST check
+                with open(one_file) as fp:
+                    tree = ast.parse(fp.read())
+                    self.check_code_ast(tree)
+
 
     def printallerrors(self):
         self.log_data = list(sorted(self.log_data.items(), key=lambda x: (x[1][0], x[1][1], x[1][2])))
         for elem in self.log_data:
-            print(f"{elem[1][0]}: Line {elem[1][1]}: {elem[1][2]} {errors_dict[elem[1][2]]}")
+            log_string = f"{elem[1][0]}: Line {elem[1][1]}: {elem[1][2]} {errors_dict[elem[1][2]]}"
+            print(log_string.replace("X===X", elem[1][3]))
 
     def print_statistics(self):
+        """Print statistic, error count and files count"""
         print("Total error count:", len(self.log_data), "in", self.filescount, "file(s)")
+
 
 
 arg_pars = argparse.ArgumentParser("The simplest Python code analyzer ever")
